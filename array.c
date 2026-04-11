6e338151caddd14f481415c9057de06f47b4eaba
Gourab Modak (spexcher) <spexcher@gmail.com>
2026-04-12 00:00:59 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   array
    	new file:   array.c
    	new file:   identifier
    	new file:   identifier.c
    	new file:   main.cpp
    	new file:   tempCodeRunnerFile.c

---
+++ array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // a sequence of same data type elements
+    // int a[6] = {78,56,75,32,23,77};
+    // printf("%d",*(a+1));
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", arr + i);
+    }
+    printf("the elements are\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d \n", *(arr + i));
+    }
+
+    return 0;
+}
---
+++ identifier.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 2;
+    age = age + 6;
+    char x = 'D';
+    printf("%d", x);
+    return 0;
+}
