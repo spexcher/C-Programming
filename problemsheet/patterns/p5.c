393d1bb976d208a7d1d9531b0cf6134a6780a6d7
Gourab Modak (spexcher) <spexcher@gmail.com>
2026-05-17 23:00:08 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   problemsheet/patterns/p5
    	new file:   problemsheet/patterns/p5.c
    	new file:   problemsheet/patterns/tempCodeRunnerFile.c

---
+++ problemsheet/patterns/p5.c
@@ -0,0 +1,38 @@
+
+
+// n=5
+//      1
+//     1 2
+//    1 2 3
+//   1 2 3 4
+//  1 2 3 4 5
+// n=5
+//      A
+//     A B
+//    A B C
+//   A B C D
+//  A B C D E
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    int k = 1;
+    scanf("%d", &n);
+    int sp = n - 1;
+    int ascA = (int)'A';
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= sp; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("%c ", (char)(ascA + k - 1));
+        }
+        sp--;
+        printf("\n");
+    }
+    return 0;
+}
