6c623017572acf5e9ee8022dbd0f5c42bd3553ab
Gourab Modak (spexcher) <spexcher@gmail.com>
2026-05-17 23:31:55 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   bitwise_operators/a
    	modified:   bitwise_operators/a.c
    	modified:   problemsheet/patterns/p5.c

--- bitwise_operators/a.c
+++ bitwise_operators/a.c
@@ -8,11 +8,10 @@ int main()
     int e = a ^ b;
     printf("AND %d OR %d XOR %d\n", c, d, e);
 
-    int rs = 23675427 >> 6;
-    printf("23675427 shifted right by 6 units is %d\n", rs);
+    int rs = 7892 >> 4;
+    printf("7892 shifted right by 4 units is %d\n", rs);
     int ls = 543 << 8;
     printf("543 shifted left by 8 units is %d\n", ls);
-
 
     return 0;
 }
