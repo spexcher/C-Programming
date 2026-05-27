df3e64407ed288e07063561b325f7f77cffb8db3
Gourab Modak (spexcher) <spexcher@gmail.com>
2026-05-28 01:39:14 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   problemsheet/selectionsort.c

--- problemsheet/selectionsort.c
+++ problemsheet/selectionsort.c
@@ -6,6 +6,7 @@ int main()
     int arr[n];
     for (int i = 0; i < n; i++)
         scanf("%d", arr + i);
+        
     for (int i = 0; i < n; i++)
     {
         int minindex = i;
@@ -27,6 +28,5 @@ int main()
     }
     for (int i = 0; i < n; i++)
         printf("%d ", *(arr + i));
-
     return 0;
 }
