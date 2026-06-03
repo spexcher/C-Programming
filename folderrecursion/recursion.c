5097d441ba4448f5b94731377ca0fddf8d9da2d8
Gourab Modak (spexcher) <spexcher@gmail.com>
2026-06-03 23:30:21 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   call_by_reference
    	modified:   call_by_value
    	new file:   folderrecursion/recursion.c

---
+++ folderrecursion/recursion.c
@@ -0,0 +1,23 @@
+// wap in input in an array and print sum of
+// elements of that array using recursion
+
+#include <stdio.h>
+
+void inputarray(int *arr, int i, int n)
+{
+    if (i < n)
+    {
+        scanf("%d", arr);
+        inputarray(arr + 1, i + 1, n);
+    }
+}
+
+int main()
+{
+    /* code */
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    inputarray(arr, 0, n);
+    return 0;
+}
